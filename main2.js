bd4445752c696cb3fd6f0f2a6970351c49dc5dad
JanLan <idiomasjanlan@gmail.com>
2024-11-11 12:23:43 -0300

    2

--- main2.js
+++ main2.js
@@ -2,3 +2,13 @@
 // Haz una función que reciba una cadena de texto ingresada por el usuario (por ejemplo, una reseña o comentario sobre la compra) y devuelva cuántos caracteres tiene la cadena (incluyendo los espacios). Muestra el resultado con un alert().
 
 // Pista: Usa la propiedad .length de las cadenas para contar los caracteres.
+
+function cadenasDeTextos(){
+    let reseña = prompt("Le gustaria dejar alguna reseña?: ");
+    if(reseña){
+        alert(reseña.length);
+    }  
+}
+cadenasDeTextos();
+
+//codigo concluido 🙂
